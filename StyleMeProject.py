021b61c7910efb7ad80c393d9bf4a3d62d14c816
Jbowden458 <jbowde28@students.kennesaw.edu>
2025-04-21 21:43:57 -0400

    Update StyleMeProject.py
    I just deleted my whole code and added in a new and improved version. After the initial code was written, I had quite a few errors. In order to properly work through all of them, I utilized Jupyter notebook. The main errors were unnecessary spaces, missing commas in the artist list, wrong indentation, misspelled words, and missing characters/symbols. For example, I was getting a tuple error on each one of my artist list because I did not have a comma after every parenthesis was closed. Instead of artist, I previously wrote artist, so python did not recognize it. There was a missing \ in front of a statement, so it was not recognized. As well as my indentation after my if statement was not right. Now I have been able to get the code to run, except the actual fashion description is not added yet. As well as, I am having an issue with the introductory statement not appearing until the end of my code.

--- StyleMeProject.py
+++ StyleMeProject.py
@@ -14,7 +14,7 @@ for i, genre in enumerate(genres, 1):
 
 while True:
     try: 
-        genre_choice = int(input("\Enter the number of your favorite genre: "))
+        genre_choice = int(input("\nEnter the number of your favorite genre: "))
         if 1 <= genre_choice <= len(genres):
             chosen_genre = genres[genre_choice - 1]
             print(f"To solidify,You picked {chosen_genre}!\n")
@@ -27,63 +27,64 @@ while True:
 # Step 3: Introduce the music genre artist choices utilizing 2 choices each gennre
 artist_choices = {
   "Hip-Hop": [
-    ("Lil Wayne", "Lil Baby")
-    ("Eminem", "J.Cole")
-    ("Dr.Dre", "Metro Boomin")
-    ("Lil Kim", "Meghan Thee Stallion")
-    ("Nicki Minaj", "Flo Milli")
+    ("Lil Wayne", "Lil Baby"),
+    ("Eminem", "J.Cole"),
+    ("Dr.Dre", "Metro Boomin"),
+    ("Lil Kim", "Meghan Thee Stallion"),
+    ("Nicki Minaj", "Flo Milli"),
   ],
   "Rock": [
-    ("Radiohead", "Red Hot Chili Peppers")
-    ("Green Day", "The White Stripes")
-    ("Nirvana", "Arctic Monkeys")
-    ("Queen", "Blink-182")
+    ("Radiohead", "Red Hot Chili Peppers"),
+    ("Green Day", "The White Stripes"),
+    ("Nirvana", "Arctic Monkeys"),
+    ("Queen", "Blink-182"),
   ],
   "Country": [
-   ("Kane Brown", "Shaboozey")
-   ("Chris Stapleton", "Luke Combs") 
-   ("Blake Shelton", "Morgan Wallon")
-   ("Dolly Parton", "Kacey Musgraves")
-   ("Reba McEntire", "Carrie Underwood")
+   ("Kane Brown", "Shaboozey"),
+   ("Chris Stapleton", "Luke Combs"), 
+   ("Blake Shelton", "Morgan Wallon"),
+   ("Dolly Parton", "Kacey Musgraves"),
+   ("Reba McEntire", "Carrie Underwood"),
   ],
-  "Pop"; [
-  ("Whitney Houston", "Beyonce")
-  ("Celine Deion", "Arianna grande")
-  ("TLC", "Destinys's, Child")
-  ("ColdPlay", "One-Direction")
-  ("Michael Jackson", "Pharrell Williams")
+  "Pop": [
+  ("Whitney Houston", "Beyonce"),
+  ("Celine Deion", "Arianna grande"),
+  ("TLC", "Destinys's, Child"),
+  ("ColdPlay", "One-Direction"),
+  ("Michael Jackson", "Pharrell Williams"),
  ],
-  "R&B"; [
-  ("Jhene Aiko", "Kehlani")
-  ("Jeremih", "Bryson Tiller")
-  ("Victoria Monet", "Muni Long")
-  ("Usher", "Chris Brown")
-  ("Alicia Keys", "Ella Mai")
+  "R&B": [
+  ("Jhene Aiko", "Kehlani"),
+  ("Jeremih", "Bryson Tiller"),
+  ("Victoria Monet", "Muni Long"),
+  ("Usher", "Chris Brown"),
+  ("Alicia Keys", "Ella Mai"),
 ],
 }
 
 chosen_artist = []
-print(f"Now there will be 5 rounds where you have to choose between two popular artist within the {chosen_genre} genre! Have Fun!")
+print(f"Now there will be {len(artist_choices.get(chosen_genre, []))} rounds where you have to choose between two popular artist within the {chosen_genre} genre! Have Fun!")
 artist_pairs = artist_choices.get(chosen_genre)
 
-if artist_pair:
-  for i, (artist1, artist 2) in enumerate(artist_pairs, 1):
-    while True: 
-      choice = input(f"Round {i}: Who do you prefer?|n(1) {artist}\n(2) 1 or 2: ")
-      if choice == "1": 
-        chosen_artists.append(artist1)
-        print(f"You chose {artist1}!/n")
-        break 
-      elif choice == "2":
-        chosen_artists.append(artist2)
-        print(f"You chose {artist2}!\n")
-      else:
-        print("Invalid choice. Please go back and enter 1 or 2.")
+if artist_pairs:
+  for i, (artist1, artist2) in enumerate(artist_pairs, 1):
+      while True: 
+          choice = input(f"Round {i}: Who do you prefer?\n(1) {artist1}\n(2) {artist2}\nEnter 1 or 2: ")
+          if choice == "1": 
+              chosen_artist.append(artist1)
+              print(f"You chose {artist1}!/n")
+              break 
+          elif choice == "2":
+              chosen_artist.append(artist2)
+              print(f"You chose {artist2}!\n")
+              break
+          else:
+              print("Invalid choice. Please go back and enter 1 or 2.")
 else: 
-  print(f"SSorry, we dont have artist choices for the {chosen_genre} genre yet."
+    print(f"Sorry, we dont have artist choices for the {chosen_genre} genre yet.")
 
 #Step 4: Style Generation (complete description will be added later)
 print("\n--- Style Generation will be put here----")
-print(f"Based on your chosen genre and artist(Genre: {Chosen_genre}, Artists: {chosen_artists}), StyleMe! will now create your new style!")
+print(f"Based on your chosen genre and artist(Genre: {chosen_genre}, Artists: {chosen_artist}), StyleMe! will now create your new style!")
 
 style_me()
